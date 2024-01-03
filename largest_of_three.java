5bc8ea47e6bac0c7cdd3390685b76a825b44f6c7
ishangupta229 <121247230+ishangupta229@users.noreply.github.com>
2024-01-03 09:22:22 +0530

    Create largest_of_three.java

---
+++ largest_of_three.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class largest_of_three{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int c =sc.nextInt();
+
+        if (a>=b && a>=c){
+            System.out.println(a +" is the largest number");
+        }else if (b>c){
+            System.out.println(b +" is the largest number");
+        }else{
+            System.out.println(c +" is the largest number");
+        }
+
+    }
+}
